e066b54791ecec7ca153a0700c6d019489333d32
flori <florianlloret13012@gmail.com>
2024-06-13 11:31:27 +0200

    corrcet page.tx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,4 @@
-//
-// Route vers la page principale du site.
-//  Source : https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
-//
-
-// Importation de la feuille de style.
 import "./page.scss";
-
-// Importation des dépendances.
 import Image from "next/image";
 import { join } from "path";
 import { lazy } from "react";
@@ -15,11 +7,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import { unstable_setRequestLocale, getTranslations } from "next-intl/server";
 
-// Importation des types.
 import type { SkillAttributes } from "@/interfaces/Skill";
 import type { ProjectAttributes } from "@/interfaces/Project";
 
-// Importation des images statiques.
 import Genio from "@/images/genio.png";
 import Domego from "@/images/domego.png";
 import Homepage from "@/images/homepage.png";
@@ -35,14 +25,11 @@ import SpaceShooter from "@/images/spaceshooter.png";
 import Voyo from "@/images/voyo.png";
 import FacepunchMonitor from "@/images/facepunchmonitor.png";
 
-// Importation des fonctions utilitaires.
 import { generateMetadata } from "./layout";
 
-// Importation des composants.
 const SkillFilter = lazy(() => import("./components/skill-filter"));
 const ContactMailer = lazy(() => import("./components/contact-mailer"));
 
-// Récupération des projets et des compétences.
 const directory = join(process.cwd(), "data");
 const getProjects = async () => JSON.parse(
   await readFile(`${directory}/projects.json`, "utf8")
@@ -52,95 +39,55 @@ const getSkills = async () => JSON.parse(
   await readFile(`${directory}/skills.json`, "utf8")
 ) as SkillAttributes[];
 
-// Récupération de l'image statique correspondant au nom d'un projet.
-//  Note : cette astuce est utilisée pour le chargement progressif des images.
-//  Source : https://nextjs.org/docs/app/api-reference/components/image#placeholder
 const getImage = (name: string) => {
   switch (name) {
-    // Genio.
     case "genio":
       return Genio;
-
-    // Domego.
     case "domego":
       return Domego;
-
-    // Homepage.
     case "homepage":
       return Homepage;
-
-    // Portfolio.
     case "portfolio":
       return Portfolio;
-
-    // Games On Web 2023.
     case "gamesonweb2023":
       return GamesOnWeb;
-
-    // Assignment Manager.
     case "assignment":
       return Assignment;
-
-    // Simple File Storage.
     case "filestorage":
       return FileStorage;
-
-    // Source Web Console.
     case "sourceconsole":
       return SourceConsole;
-
-    // Manga Paradise.
     case "mangaparadise":
       return MangaParadise;
-
-    // Raven Framework.
     case "platformerunity":
       return PlatformerUnity;
-
-    // Digital Identity.
     case "digitalidentity":
       return DigitalIdentity;
-
-    // Steam Collection Download Size Calculator
     case "spaceshooter":
       return SpaceShooter;
-
     case "voyo":
       return Voyo;
-
-    // Facepunch Commits Monitor
     case "facepunchmonitor":
       return FacepunchMonitor;
-
-    // Aucune image.
     default:
       return "";
   }
 };
 
-// Affichage de la page.
 export default async function Page({
   params: { locale }
 }: {
   params: { locale: string };
 }) {
-  // Définition de la langue de la page.
   unstable_setRequestLocale(locale);
-
-  // Déclaration des variables d'état.
   const t = await getTranslations();
-
-  // Déclaration des constantes.
   const github = (await generateMetadata()).source;
   const date = new Date();
 
   date.setTime(date.getTime() - Date.parse("08 Aug 1999 00:00:00 GMT"));
 
-  // Affichage du rendu HTML de la page.
   return (
     <main>
-      {/* Affichage de l'animation du logo vers le dépôt GitHub */}
-      {/* Source : https://tholman.com/github-corners/ */}
       <a
         rel="noopener noreferrer"
         href={github}
@@ -152,14 +99,13 @@ export default async function Page({
           <path d="M128 109c-15-9-9-19-9-19 3-7 2-11 2-11-1-7 3-2 3-2 4 5 2 11 2 11-3 10 5 15 9 16" />
           <path
             d="M115 115s4 2 5 0l14-14c3-2 6-3 8-3-8-11-15-24 2-41 5-5 10-7 16-7 1-2 3-7 12-11 0 0
-						5 3 7 16 4 2 8 5 12 9s7 8 9 12c14 3 17 7 17 7-4 8-9 11-11 11 0 6-2 11-7 16-16 16-30 10-41
-						2 0 3-1 7-5 11l-12 11c-1 1 1 5 1 5z"
+            5 3 7 16 4 2 8 5 12 9s7 8 9 12c14 3 17 7 17 7-4 8-9 11-11 11 0 6-2 11-7 16-16 16-30 10-41
+            2 0 3-1 7-5 11l-12 11c-1 1 1 5 1 5z"
           />
         </svg>
       </a>
 
       <section>
-        {/* Prénom et nom du développeur */}
         <h1>
           {t("landing.hello_title")}
           <span>{t("landing.developer_firstname")}</span>
@@ -167,14 +113,12 @@ export default async function Page({
         </h1>
 
         <article id="about">
-          {/* Résumé du développeur */}
           <p>
             {t("landing.developer_description", {
               age: date.getFullYear() - 1970
             })}
           </p>
 
-          {/* Bouton de téléchargement du CV */}
           <a
             rel="noopener noreferrer"
             href="https://drive.google.com/file/d/18Z_OUsALC393t8oVAS3BIxcYP8GzbGrp/view"
@@ -186,14 +130,11 @@ export default async function Page({
       </section>
 
       <section id="projects">
-        {/* Section des projets */}
         <h2>{t("landing.header_projects")}</h2>
 
-        {/* Génération des projets */}
         <ul>
           {Object.entries(await getProjects()).map(([key, value]) => (
             <li key={key}>
-              {/* Image du projet */}
               <Image
                 src={getImage(key)}
                 alt={value.title}
@@ -202,54 +143,39 @@ export default async function Page({
                 placeholder="blur"
               />
 
-              {/* Contenu du projet */}
               <div>
-                {/* Titre du projet */}
                 <h3>{value.title}</h3>
-
-                {/* Description du projet */}
                 <p>{t(`projects.${key}`)}</p>
-
-                {/* Compétences utilisées pour le projet */}
                 <ul>
                   {value.skills.map((skill) => (
                     <li key={skill}>{skill}</li>
                   ))}
                 </ul>
-
-                {/* Liens du projet */}
                 <ul>
-                  {
-                    // Dépôt Git (facultatif).
-                    value.repository && (
-                      <li>
-                        <a
-                          rel="noopener noreferrer"
-                          href={value.repository}
-                          target="_blank"
-                          aria-label={t("landing.project_source")}
-                        >
-                          <FontAwesomeIcon icon={faCode} />
-                        </a>
-                      </li>
-                    )
-                  }
-
-                  {
-                    // Site de démonstration (facultatif).
-                    value.demo && (
-                      <li>
-                        <a
-                          rel="noopener noreferrer"
-                          href={value.demo}
-                          target="_blank"
-                          aria-label={t("landing.project_demo")}
-                        >
-                          <FontAwesomeIcon icon={faExternalLinkAlt} />
-                        </a>
-                      </li>
-                    )
-                  }
+                  {value.repository && (
+                    <li>
+                      <a
+                        rel="noopener noreferrer"
+                        href={value.repository}
+                        target="_blank"
+                        aria-label={t("landing.project_source")}
+                      >
+                        <FontAwesomeIcon icon={faCode} />
+                      </a>
+                    </li>
+                  )}
+                  {value.demo && (
+                    <li>
+                      <a
+                        rel="noopener noreferrer"
+                        href={value.demo}
+                        target="_blank"
+                        aria-label={t("landing.project_demo")}
+                      >
+                        <FontAwesomeIcon icon={faExternalLinkAlt} />
+                      </a>
+                    </li>
+                  )}
                 </ul>
               </div>
             </li>
@@ -257,10 +183,7 @@ export default async function Page({
         </ul>
       </section>
 
-      {/* Section des compétences */}
       <SkillFilter skills={await getSkills()} />
-
-      {/* Section de contact */}
       <ContactMailer />
     </main>
   );
